2fe7cc1699e6dd6d87b1659dae46607d5bae3d05
kulloveth <kulloveth@gmail.com>
2021-10-04 07:37:03 +0100

    enable location on device

--- app/src/main/java/eu/tutorials/weatherapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/weatherapp/MainActivity.kt
@@ -1,11 +1,53 @@
 package eu.tutorials.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO (STEP 6: Check here whether GPS is ON or OFF using the method which we have created)
+        // START
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            // This will redirect you to settings from where you need to turn on the location provider.
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this,
+                "Your location provider is already ON.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        // END
     }
+
+    // TODO (STEP 5: Check whether the GPS is ON or OFF)
+    // START
+    /**
+     * A function which is used to verify that the location or GPS is enabled or not.
+     */
+    private fun isLocationEnabled(): Boolean {
+
+        // This provides access to the system location services.
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    // END
 }
