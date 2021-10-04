f6b582bbabdc6b288f32cbc101ea73cd3c0f130e
kulloveth <kulloveth@gmail.com>
2021-10-04 08:21:40 +0100

    get current location latitude and longitude

--- app/src/main/java/eu/tutorials/weatherapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/weatherapp/MainActivity.kt
@@ -1,20 +1,48 @@
 package eu.tutorials.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
+// OpenWeather Link : https://openweathermap.org/api
+/**
+ * The useful link or some more explanation for this app you can checkout this link :
+ * https://medium.com/@sasude9/basic-android-weather-app-6a7c0855caf4
+ */
 class MainActivity : AppCompatActivity() {
+
+    // TODO (STEP 3: Add a variable for FusedLocationProviderClient.)
+    // START
+    // A fused location client variable which is further used to get the user's current location
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    // END
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO (STEP 6: Check here whether GPS is ON or OFF using the method which we have created)
+        // TODO (STEP 4: Initialize the fusedLocationProviderClient variable.)
         // START
+        // Initialize the Fused location variable
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        // END
         if (!isLocationEnabled()) {
             Toast.makeText(
                 this,
@@ -26,17 +54,104 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
         } else {
-            Toast.makeText(
-                this,
-                "Your location provider is already ON.",
-                Toast.LENGTH_SHORT
-            ).show()
+            // TODO (STEP 1: Asking the location permission on runtime.)
+            // START
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            // TODO (STEP 7: Call the location request function here.)
+                            requestLocationData()
+
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "You have denied location permission. Please allow it is mandatory.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+            // END
         }
-        // END
+
     }
 
-    // TODO (STEP 5: Check whether the GPS is ON or OFF)
+    // TODO (STEP 5: Add a function to get the location of the device using the fusedLocationProviderClient.)
     // START
+    /**
+     * A function to request the current location. Using the fused location provider client.
+     */
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+
+        val mLocationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+    // END
+    // TODO (STEP 6: Register a request location callback to get the location.)
+    // START
+    /**
+     * A location callback object of fused location provider client where we will get the current location details.
+     */
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current Latitude", "$latitude")
+
+            val longitude = mLastLocation.longitude
+            Log.i("Current Longitude", "$longitude")
+        }
+    }
+    // END
+
+// TODO (STEP 2: A alert dialog for denied permissions and if needed to allow it from the settings app info.)
+    // START
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
+    // END
     /**
      * A function which is used to verify that the location or GPS is enabled or not.
      */
@@ -49,5 +164,4 @@ class MainActivity : AppCompatActivity() {
             LocationManager.NETWORK_PROVIDER
         )
     }
-    // END
 }
